f90f8600b28466da6fa4b1aa37c7f6cae9bcb129
HaiMing.Yin <HaiMing.Yin@gmail.com>
2015-03-24 01:16:04 +0800

    make a definitive last key can not make SeekToLast work neither

--- slice_transform_test.go
+++ slice_transform_test.go
@@ -1,11 +1,53 @@
 package gorocksdb
 
 import (
+	"log"
 	"os"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
+
+var (
+	rdb        *DB
+	rdbClosed  bool
+	dbpath     string
+	rdbOptions *Options
+)
+
+func setup() {
+	dbpath := os.TempDir() + "/TestSliceTransformInconsistency"
+	transform := NewFixedPrefixTransform(3)
+
+	rdbOptions := NewDefaultOptions()
+	rdbOptions.SetPrefixExtractor(transform)
+	rdbOptions.SetHashSkipListRep(50000, 4, 4)
+	rdbOptions.SetAllowMmapReads(true)
+	rdbOptions.SetAllowMmapWrites(true)
+	rdbOptions.SetPlainTableFactory(4, 10, 0.75, 16)
+	rdbOptions.SetCreateIfMissing(true)
+
+	var err error
+	rdb, err = OpenDb(rdbOptions, dbpath)
+	if err != nil {
+		log.Printf("Setup error, can not opendb: %s", err)
+	}
+}
+
+func teardown() {
+	if !rdbClosed {
+		closeDB()
+		err := DestroyDb(dbpath, rdbOptions)
+		if err != nil {
+			log.Printf("fail on destroy rdb: %s", err)
+		}
+	}
+}
+
+func closeDB() {
+	rdb.Close()
+	rdbClosed = true
+}
 
 type testSliceTransform struct {
 	initiated bool
@@ -143,5 +185,76 @@ func TestFixedPrefixTransform(t *testing.T) {
 
 		So(it.Err(), ShouldBeNil)
 		So(numFound, ShouldEqual, 3)
+	})
+}
+
+func TestFixedPrefixTransformWithMax(t *testing.T) {
+	setup()
+	defer teardown()
+
+	Convey("Subject: Prefix filtering with custom slice transform", t, func() {
+		Convey("The old Iteration", func() {
+			wo := NewDefaultWriteOptions()
+
+			maxKey := []byte{
+				0xFF, 0xFF, 0xFF, 0xFF,
+			}
+			So(rdb.Put(wo, maxKey, []byte("")), ShouldBeNil)
+
+			So(rdb.Put(wo, []byte("foo1"), []byte("foo")), ShouldBeNil)
+			So(rdb.Put(wo, []byte("foo2"), []byte("foo")), ShouldBeNil)
+			So(rdb.Put(wo, []byte("foo3"), []byte("foo")), ShouldBeNil)
+			So(rdb.Put(wo, []byte("bar1"), []byte("bar")), ShouldBeNil)
+			So(rdb.Put(wo, []byte("bar2"), []byte("bar")), ShouldBeNil)
+			So(rdb.Put(wo, []byte("bar3"), []byte("bar")), ShouldBeNil)
+
+			ro := NewDefaultReadOptions()
+
+			it := rdb.NewIterator(ro)
+			defer it.Close()
+			numFound := 0
+			for it.Seek([]byte("bar")); it.Valid(); it.Next() {
+				numFound++
+			}
+
+			So(it.Err(), ShouldBeNil)
+			So(numFound, ShouldEqual, 3)
+		})
+
+		Convey("Iteration without destroy first, reopen old db", func() {
+			// reopen
+			closeDB()
+			setup()
+
+			ro := NewDefaultReadOptions()
+			it := rdb.NewIterator(ro)
+			defer it.Close()
+			numFound := 0
+			for it.Seek([]byte("bar")); it.Valid(); it.Next() {
+				numFound++
+			}
+
+			So(it.Err(), ShouldBeNil)
+			So(numFound, ShouldEqual, 3)
+		})
+
+		Convey("SeekToLast after reopen ", func() {
+			// reopen
+			closeDB()
+			setup()
+			defer teardown()
+
+			ro := NewDefaultReadOptions()
+			it := rdb.NewIterator(ro)
+			defer it.Close()
+			numFound := 0
+			it.Seek([]byte("bar"))
+			for it.SeekToLast(); it.Valid(); it.Prev() {
+				numFound++
+			}
+
+			So(it.Err(), ShouldBeNil)
+			So(numFound, ShouldEqual, 3)
+		})
 	})
 }
